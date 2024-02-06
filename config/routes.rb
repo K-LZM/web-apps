7aee0f5747ef437759d812ff9fe83fa6683f6291
Kevin Liao <kevin.liao@kellogg.northwestern.edu>
2024-02-06 16:49:55 +0000

    working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
+# create the route for webpage
+# rails runner xxxx
+# rails db:migrate
+# rails server
+# rails generate controller xxx
+
 Rails.application.routes.draw do
-  
+  get "/tacos", :controller => "tacos", :action => "index"
+  get "/dice", :controller => "dice", :action => "index" 
 end
